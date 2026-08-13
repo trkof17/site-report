9ed8a82617c166e7cf26ddc7b524e1d3d93f950a
trkof17 <tarik.aydin@sarcon.com.tr>
2026-08-13 21:03:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 12 03:53:57 2026
-
-@author: taric
-"""
-
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")    
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
